026779ee9b84ed770b9cb5313fa82a2da835d144
mjcry <mj.masilo.jack@gmail.com>
2022-09-08 00:30:15 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 /* This is program that prints out without printf */
 #include <stdio.h>
 
-int main ()
+int main (void)
 {
- 	puts("Programming is like building a multilingual puzzle,\n");	
+ 	puts("Programming is like building a multilingual puzzle\n");	
 	return 0;
 }
 
