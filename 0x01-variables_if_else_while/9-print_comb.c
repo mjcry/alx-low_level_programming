3f0b5aef4df0e4b37796b23482edffbb0293e0d6
mjcry <mj.masilo.jack@gmail.com>
2022-09-09 06:57:53 -0700

    var

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 /**
- *
  * main - entry main fuction
  *
- * return - return to zero (sucess)
+ * int - integer is called as variable caleled num
+ *
+ * while - to repeat the numbers till we get 9
+ *
+ * putchar - used to decaler number and to print it out
+ *
+ * if - if the number is inverse and = 9 the output
+ *
+ * num++ - is a statement
+ *
+ * return - return to 0 t0 9 decimals (sucess)
  */
 int main(void)
 {
-	int num
-	
-	num = '0';
+	int num = '0';
 
 	while (num <= '9')
 	{
@@ -23,5 +30,5 @@ int main(void)
 	}
 
 	putchar(10);
-	return(0);
+	return (0);
 }
