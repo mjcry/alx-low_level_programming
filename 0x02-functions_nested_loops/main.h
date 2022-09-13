4349c0d1425ec26428d57c0dd2e81c435a34b67c
mjcry <mj.masilo.jack@gmail.com>
2022-09-13 03:51:43 -0700

    f

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,18 @@
-#ifdef HOLBERTON_H
+#ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-char _putchar(char c);
+char print_alphabet(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int nld);
+int print_last_digit(int);
 void jack_bauer(void);
-void time_table(void);
-int add(int one, int two);
+void times_table(void);
+int add(int, int);
+int add(int, int);
 void print_to_98(int n);
-void print_times_tables(int n);
 
 #endif
