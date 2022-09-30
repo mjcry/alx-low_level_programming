2b66efcbbd3918a9d611fb02f7b5de084ef27f7b
mjcry <mj.masilo.jack@gmail.com>
2022-09-30 05:33:28 -0700

    q

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - prints it name plus linebreak
  *
@@ -11,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	argc = 0;
+	(void)argc;
 
-	printf("%s\n, argv[argc]");
+	printf("%s\n, argv[0]");
 	return (0);
 }
