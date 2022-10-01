1aee14eddc0a682f4cba934bfd5d157b04611f7e
mjcry <mj.masilo.jack@gmail.com>
2022-09-30 21:00:49 -0700

    h

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("Hello");
-	return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
